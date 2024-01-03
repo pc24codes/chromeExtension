c6c26409d21988bcf1b41a4ec498bb33219318bf
parasuramc <parasuramreddy2000@gmail.com>
2024-01-04 03:09:47 +0530

    Added new lines of code.

--- index.js
+++ index.js
@@ -2,10 +2,42 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn") 
+const ulEl = document.getElementById("ul-el")
 
-//using event-listeners instead of logging
+let name = localStorage.setItem("myLeads", "www.facebook.com")
+localStorage.getItem("myLeads")
+
+//using event-listeners instead of console.log
 inputBtn.addEventListener('click', function(){
    // console.log('Button clicked using eventlistener')
+
     myLeads.push(inputEl.value) // this will take the input and log in the console post the button-click
-    console.log(myLeads)
+    inputEl.value = "" // this will clear the textbox post the button click
+    renderLeads()
+    
 })
+
+
+function renderLeads(){
+    let listItems = ""
+
+    for(i=0; i<myLeads.length; i++ )
+    {
+        console.log(myLeads[i])
+        //ulEl.textContent +=  myLeads[i] + " " // this will render as plain text 
+        // listItems += "<li><a target = '_blank' href = '#'>"+myLeads[i] +"</li>" // will render as ordered items as a hyperlink
+        //console.log(listItems)
+        listItems += 
+            `<li>
+            <a target='_blank' href='${myLeads[i]}'> 
+            ${myLeads[i]}    
+            </a>
+            </li>`  // this is a template string
+
+       /* const li = document.createElement("li")
+        li.textContent = myLeads[i]
+        ulEl.append(li)
+        */
+    }
+    ulEl.innerHTML = listItems
+}
