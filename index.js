3975f521c7ddd3f8c6dce66eac732fc4d290d0ed
parasuramc <parasuramreddy2000@gmail.com>
2024-01-07 01:27:55 +0530

    added local storage logic

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-//variables
-let myLeads = []
+//variable pool
+let myLeads = [] 
+
+// //notes 
+//pushing a new value 
+// myLeads.push("1234566988")
+
+// //aray to string
+// myLeads = JSON.stringify(myLeads)
+
+// console.log(myLeads) //prints the value
+// console.log(typeof myLeads) //string
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn") 
 const ulEl = document.getElementById("ul-el")
 
-let name = localStorage.setItem("myLeads", "www.facebook.com")
-localStorage.getItem("myLeads")
+let leadsfromLocal = JSON.parse(localStorage.getItem("myLeads"))
+console.log("local leads: "+ leadsfromLocal)
 
+ 
 //using event-listeners instead of console.log
 inputBtn.addEventListener('click', function(){
    // console.log('Button clicked using eventlistener')
 
     myLeads.push(inputEl.value) // this will take the input and log in the console post the button-click
     inputEl.value = "" // this will clear the textbox post the button click
+    
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))  //localstorage will save the details; accepts key value pairs
+    //JSON.string is used inorder to change the array to string.
+    
     renderLeads()
     
+    console.log(localStorage.getItem("myLeads"))
 })
 
 
